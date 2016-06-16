e0ffb8cbd2370ce62845cea8a7cd781e9afd9f30
bhuiya16 <bhuiya16@UTORONTO.CA>
2016-06-16 22:53:16 +0000

    update sprint1, 2 logs to update tasks, created shell functions class

---
+++ src/driver/ShellFunctions.java
@@ -0,0 +1,51 @@
+package driver;
+
+public class ShellFunctions {
+
+	public ShellFunctions() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// COULD MAKE EACH METHOD PROTECTED,
+	// CALL COMMANDS TO DIFFERENT CLASSES HERE
+	
+	public static String mkdir (String directory) {
+		return null;
+	}
+	
+	public static String cd (String directory) {
+		return null;
+	}
+	
+	public static String ls (String path) {
+		return null;
+	}
+	
+	public static String pwd () {
+		return null;
+	}
+	
+	public static String pushd (String directory) {
+		return null;
+	}
+	
+	public static String popd () {
+		return null;
+	}
+	
+	public static String history (int max) {
+		return null;
+	}
+	
+	public static String cat (String[] files) {
+		return null;
+	}
+
+	public static String echo (String outfile) {
+		return null;
+	}
+	
+	public static String man (String command) {
+		return null;
+	}
+}
