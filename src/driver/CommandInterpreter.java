efa98a3656f8f0e6a35e311ea6792a19c8fa8660
pateld59 <pateld59@UTORONTO.CA>
2016-06-22 16:10:10 +0000

    Added temp generalshelltest

--- src/driver/CommandInterpreter.java
+++ src/driver/CommandInterpreter.java
@@ -3,7 +3,6 @@ package driver;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class CommandInterpreter {
 	private ShellFunctions functions;
