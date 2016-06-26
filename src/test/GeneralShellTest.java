3d439976920b1c5c5060da7d3c42e7fb43ec2bd9
John <JohnSongSoftware@users.noreply.github.com>
2016-06-26 16:21:13 +0000

    Changed tests to run with new design.

--- src/test/GeneralShellTest.java
+++ src/test/GeneralShellTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import driver.CommandInterpreter;
 import driver.Directory;
 import driver.Directory.InvalidAddition;
 import driver.Directory.MissingNameException;
@@ -13,120 +12,111 @@ import driver.File;
 import driver.FilePathInterpreter;
 import driver.FilePathInterpreter.InvalidDirectoryPathException;
 import driver.FileTypes;
+import driver.JShell;
 import driver.MySession;
 
 public class GeneralShellTest {
 
 	@Test
 	public void changeDirectory() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
+		MySession session = new MySession();
 		
-		System.out.println(cmd.interpretCmd("mkdir usr"));
-		System.out.println(cmd.interpretCmd("mkdir /hello"));
-		System.out.println(cmd.interpretCmd("ls"));
-		System.out.println(cmd.interpretCmd("cd usr/"));
-		System.out.println(cmd.interpretCmd("pwd"));
-		System.out.println(cmd.interpretCmd("mkdir john"));
-		System.out.println(cmd.interpretCmd("mkdir /test"));
-		System.out.println(cmd.interpretCmd("ls"));
-		System.out.println(cmd.interpretCmd("cd /"));
-		System.out.println(cmd.interpretCmd("ls"));
-		System.out.println(cmd.interpretCmd("mkdir hello/hi"));
-		System.out.println(cmd.interpretCmd("cd ."));
-		System.out.println(cmd.interpretCmd("ls"));
-		System.out.println(sess.getCurrentDir().getChildren().toString());
-		cmd.interpretCmd("cd hello");
-		System.out.println(cmd.interpretCmd("ls"));
+		System.out.println(JShell.commandProcessor("mkdir usr", session));
+		System.out.println(JShell.commandProcessor("mkdir /hello", session));
+		System.out.println(JShell.commandProcessor("ls", session));
+		System.out.println(JShell.commandProcessor("cd usr/", session));
+		System.out.println(JShell.commandProcessor("pwd", session));
+		System.out.println(JShell.commandProcessor("mkdir john", session));
+		System.out.println(JShell.commandProcessor("mkdir /test", session));
+		System.out.println(JShell.commandProcessor("ls", session));
+		System.out.println(JShell.commandProcessor("cd /", session));
+		System.out.println(JShell.commandProcessor("ls", session));
+		System.out.println(JShell.commandProcessor("mkdir hello/hi", session));
+		System.out.println(JShell.commandProcessor("cd .", session));
+		System.out.println(JShell.commandProcessor("ls", session));
+		System.out.println(session.getCurrentDir().getChildren().toString());
+		JShell.commandProcessor("cd hello", session);
+		System.out.println(JShell.commandProcessor("ls", session));
 		
-		//System.out.println(sess.getCurrentDir().getChild("hi"));
-		System.out.println(sess.getCurrentDir().getChildren().toString());
+		//System.out.println(session.getCurrentDir().getChild("hi"));
+		System.out.println(session.getCurrentDir().getChildren().toString());
 	}
 
 	@Test
 	public void mkdirMult() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
+		MySession session = new MySession();
+		JShell.commandProcessor("mkdir usr usr/hello /usr/hello/bye", session);
 
-		cmd.interpretCmd("mkdir usr usr/hello /usr/hello/bye");
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("cd usr", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		cmd.interpretCmd("cd usr");
-		System.out.println(sess.getCurrentDir().getChildNames());
-
-		cmd.interpretCmd("cd hello");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("cd hello", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 	}
 
 	@Test
 	public void cdParent() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
+		MySession session = new MySession();
 
-		cmd.interpretCmd("mkdir usr usr/hello /usr/hello/bye");
-		cmd.interpretCmd("cd ..");//Should remain at the root node in this case
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("mkdir usr usr/hello /usr/hello/bye", session);
+		JShell.commandProcessor("cd ..", session);//Should remain at the root node in this case
+		System.out.println(session.getCurrentDir().getChildNames());
 	}
 
 	@Test
 	public void cdParentTwo() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
+		MySession session = new MySession();
 
-		cmd.interpretCmd("mkdir usr usr/hello /usr/hello/bye");
-		cmd.interpretCmd("cd usr/..");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("mkdir usr usr/hello /usr/hello/bye", session);
+		JShell.commandProcessor("cd usr/..", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
 	}
 
 	@Test
 	public void cdCurrent() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
+		MySession session = new MySession();
 
-		cmd.interpretCmd("mkdir usr usr/hello /usr/hello/bye");
-		cmd.interpretCmd("cd usr/.");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("mkdir usr usr/hello /usr/hello/bye", session);
+		JShell.commandProcessor("cd usr/.", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
 	}
 
 	@Test
 	public void mkDirDots() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
+		MySession session = new MySession();
 
-		cmd.interpretCmd("mkdir usr usr/hello /usr/hello/bye");
+		JShell.commandProcessor("mkdir usr usr/hello /usr/hello/bye", session);
 
-		System.out.println(sess.getCurrentDir().getChildNames());
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		cmd.interpretCmd("cd usr");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("cd usr", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		cmd.interpretCmd("cd hello");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("cd hello", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
 	}
 
 	@Test
 	public void mkDirFromChild() throws NameExistsException, InvalidAddition, InvalidDirectoryPathException, MissingNameException{
-		MySession sess = new MySession();
-		CommandInterpreter cmd = new CommandInterpreter(sess);
-
+		MySession session = new MySession();
 		
-		System.out.println("HI");
-		cmd.interpretCmd("mkdir usr");
+		JShell.commandProcessor("mkdir usr", session);
 
-		System.out.println(sess.getCurrentDir().getChildNames());
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		cmd.interpretCmd("cd usr");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("cd usr", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		cmd.interpretCmd("mkdir /hello");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("mkdir /hello", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 
-		cmd.interpretCmd("cd ..");
-		System.out.println(sess.getCurrentDir().getChildNames());
+		JShell.commandProcessor("cd ..", session);
+		System.out.println(session.getCurrentDir().getChildNames());
 		System.out.println("ls");
 
 	}
