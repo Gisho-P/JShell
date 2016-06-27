82004974e9151a4cce08486e3718d7f6613a6b7a
pushpa10 <pushpa10@UTORONTO.CA>
2016-06-27 23:50:06 +0000

    Separated displayStoreString's exec by adding a helper, modified method names for tests to include test prefix and changed mysessiontest to gethistorytest

--- src/test/GetHistoryTest.java
+++ src/test/GetHistoryTest.java
@@ -5,37 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import driver.MySession;
 
-public class MySessionTest {
+public class GetHistoryTest {
 
 	/**
 	 * Saves 5 commands to the session, and gets all history to ensure it
 	 * returns the string in the correct format & with the right content.
 	 */
 	@Test
-	public void getHistoryAll(){
+	public void testGetHistoryAll(){
 	  MySession session = new MySession();
 	  String expectedHistory = "";
 	  for(Integer i = 0; i < 5; i++){
 	    session.saveCommand("echo \"cmd " + i.toString()  + "\" > newFile");
 	    if(i != 4)
-	      expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" > newFile")) + "\n";
+	      expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" >]"
+	          + "newFile")) + "\n";
 	    else
-          expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" > newFile"));
+          expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\""
+              + "> newFile"));
 	  }
 	  assertTrue(session.printCommandHistory().equals(expectedHistory));
 	}
 	
 	@Test
-        public void getHistoryLastTwo(){
+        public void testGetHistoryLastTwo(){
 	    MySession session = new MySession();
 	    String expectedHistory = "";
 	    for(Integer i = 0; i < 5; i++){
 		session.saveCommand("echo \"cmd " + i.toString()  + "\" > newFile");
 		if(i > 2)
 		    if(i != 4)
-			expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" > newFile")) + "\n";
+			expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" >"
+			    + "newFile")) + "\n";
 		    else
-			expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" > newFile"));
+			expectedHistory += ((i + 1) + (". " + "echo \"cmd " + i  + "\" >"
+			    + "newFile"));
 	    	}
 	    assertTrue(session.printCommandHistory(2).equals(expectedHistory));
         }
