08d96d9b669e33b5f59286e28ca7b8f61976a113
songjin8@UTORONTO.CA <songjin8@UTORONTO.CA@9bafc4ea-6ec9-488b-8926-2cf38d664e2c>
2016-06-28 01:37:50 +0000

    Shortened the FilePathInterpreter and seperated methods, added documentation to ChangeDirectory.

--- Assignment2/src/driver/FilePathInterpreter.java
+++ Assignment2/src/driver/FilePathInterpreter.java
@@ -5,209 +5,284 @@ import java.util.Iterator;
 
 public class FilePathInterpreter {
 
-    /**
-     * Finds the location and returns the existing dir with the input path.
-     *
-     * @param init The init dir to move from.
-     * @param path The path relative to the init dir.
-     * @return The dir at that valid path.
-     * @throws InvalidDirectoryPathException
-     */
-    public static FileTypes interpretPath(Directory init, String path)
-            throws InvalidDirectoryPathException {
-        if (!path.equals("")) {
+	/**
+	 * Finds the location and returns the existing dir with the input path.
+	 *
+	 * @param init
+	 *            The init dir to move from.
+	 * @param path
+	 *            The path relative to the init dir.
+	 * @return The dir at that valid path.
+	 * @throws InvalidDirectoryPathException
+	 */
+	public static FileTypes interpretPath(Directory init, String path)
+			throws InvalidDirectoryPathException {
+		if (!path.equals("")) {
 
-            // Splits our path up
-            String[] splitPath = path.split("/");
+			// Splits our path up
+			String[] splitPath = path.split("/");
 
-            // Checks to see if our first is "/" i.e. is it back to root?
-            if (path.charAt(0) == '/') {
-                while (init.getParent() != null) {
-                    init = init.getParent();
-                }
-                return interpretPathRecursive(init,
-                        path.substring(1, path.length()));
-            }
+			// Checks to see if our first is "/" i.e. is it back to root?
+			if (path.charAt(0) == '/') {
+				while (init.getParent() != null) {
+					init = init.getParent();
+				}
+				return interpretPathRecursive(init,
+						path.substring(1, path.length()));
+			}
 
-            return interpretPathRecursive(init, path);
-        }
-        return init;
-    }
+			return interpretPathRecursive(init, path);
+		}
+		return init;
+	}
 
-    ;
+	/**
+	 * Recursively goes through a file path and finds the dir of file at that
+	 * path.
+	 *
+	 * @param init
+	 *            The current dir the path is relative to.
+	 * @param currPath
+	 *            The current path of the dir/file.
+	 * @return The dir or file at that path.
+	 * @throws InvalidDirectoryPathException
+	 */
+	private static FileTypes interpretPathRecursive(Directory init,
+			String currPath) throws InvalidDirectoryPathException {
+		// Checks to see whether the current path is nothing if it is we found
+		currPath = removeLeadingSlashes(currPath);
+		if (!currPath.equals("")) {
+			String[] splitPath = currPath.split("/");
+			// Same case as before except with ..
+			if (splitPath[0].equals("..")) {
+				return interpretDoubleDots(init, currPath, splitPath);
+			}
+			// Just return itself
+			if (splitPath[0].equals(".")) {
+				return interpretSingleDot(init, currPath, splitPath);
+			}
+			int index = init.nameExists(splitPath[0]);
+			// If it doesn't exist
+			if(index == -1){
+				throw new InvalidDirectoryPathException(
+						"There are no files or directories with name " + currPath);
+			}
+			if(init.getChildren().get(index) instanceof Directory){
+				return getSubDirFromDir(init, currPath, splitPath);
+			}
+			// Else it's a file
+			return getFileFromDir(init, currPath, splitPath);
+		}
+		return init;
+	}
+	
+	/**
+	 * Returns the file from a subdir.
+	 * 
+	 * @param init
+	 *            The dir the file is found at.
+	 * @param currPath
+	 *            The current path of the file.
+	 * @param splitPath
+	 *            An array split from currPath with the '/' delimiter;
+	 * @return A file with the given path relative to init;
+	 * @throws InvalidDirectoryPathException
+	 */
+	private static FileTypes getFileFromDir(Directory init, String currPath,
+			String[] splitPath) throws InvalidDirectoryPathException {
+		ArrayList<File> files = init.getChildFiles();
+		Iterator<File> filesIterator = files.iterator();
 
-    /**
-     * Recursively goes through a file path and finds the dir of file at that path.
-     *
-     * @param init     The current dir the path is relative to.
-     * @param currPath The current path of the dir/file.
-     * @return The dir or file at that path.
-     * @throws InvalidDirectoryPathException
-     */
-    private static FileTypes interpretPathRecursive(Directory init,
-                                                    String currPath) throws InvalidDirectoryPathException {
-        // Checks to see whether the current path is nothing if it is we found
-        // our init
+		// Else it's a file we're looking for
+		while (filesIterator.hasNext()) {
+			File next = filesIterator.next();
 
-        currPath = removeLeadingSlashes(currPath);
-        if (!currPath.equals("")) {
+			if (!(currPath.lastIndexOf('/') == currPath.length() - 1)) {
+				if (next.getName().equals(splitPath[0])) {
+					return next;
+				}
+			}
+		}
 
-            String[] splitPath = currPath.split("/");
-            // Same case as before except with ..
-            if (splitPath[0].equals("..")) {
-                // Checking to see if we're at the current root of not
-                if (init.getParent() != null) {
-                    // Checking if we have '../moredirs/evenmore' or just '..'
-                    if (splitPath.length > 1) {
-                        return interpretPathRecursive(init.getParent(),
-                                currPath.substring(3, currPath.length()));
-                    } else {
-                        return interpretPathRecursive(init.getParent(),
-                                currPath.substring(2, currPath.length()));
-                    }
-                } else {
-                    // Checking if we have '../moredirs/evenmore' or just '..'
-                    if (splitPath.length > 1) {
-                        return interpretPathRecursive(init,
-                                currPath.substring(3, currPath.length()));
-                    } else {
-                        return interpretPathRecursive(init,
-                                currPath.substring(2, currPath.length()));
-                    }
-                }
-            }
+		// Else it doesn't exist and throw an exception
+		throw new InvalidDirectoryPathException(
+				"There are no files or directories with name " + currPath);
+	}
+	
+	/**
+	 * Returns a subdir within another dir and calls interpret path recursively.
+	 * 
+	 * @param init
+	 *            The dir the subdir is found at.
+	 * @param currPath
+	 *            The current path of the subdir.
+	 * @param splitPath
+	 *            An array split from currPath with the '/' delimiter;
+	 * @return A file with the given path relative to init;
+	 * @throws InvalidDirectoryPathException
+	 */
+	private static FileTypes getSubDirFromDir(Directory init, String currPath,
+			String[] splitPath) throws InvalidDirectoryPathException {
+		// Checking sub dirs
+		ArrayList<Directory> subDirs = init.getChildDirs();
+		Iterator<Directory> dirIterator = subDirs.iterator();
 
-            // Just return itself
-            if (splitPath[0].equals(".")) {
-                // Checking if we have './moredirs/evenmore' or just '.'
-                if (splitPath.length > 1) {
-                    return interpretPathRecursive(init,
-                            currPath.substring(2, currPath.length()));
-                } else {
-                    return interpretPathRecursive(init,
-                            currPath.substring(1, currPath.length()));
-                }
-            }
+		// Looping over to see if what we're looking for is a dir
+		while (dirIterator.hasNext()) {
+			Directory next = dirIterator.next();
+			if (next.getName().equals(splitPath[0])) {
 
-            // Checking sub dirs
-            ArrayList<Directory> subDirs = init.getChildDirs();
-            Iterator<Directory> dirIterator = subDirs.iterator();
+				int length = splitPath[0].length();
+				// Go into the new dir
+				// Taking care of the '/' in case there are other sub dirs
 
-            // Looping over to see if what we're looking for is a dir
-            while (dirIterator.hasNext()) {
-                Directory next = dirIterator.next();
-                if (next.getName().equals(splitPath[0])) {
+				if (splitPath.length == 1)
+					return interpretPathRecursive(next,
+							currPath.substring(length, currPath.length()));
+				else
+					return interpretPathRecursive(next,
+							currPath.substring(length + 1, currPath.length()));
+			}
+		}
 
-                    int length = splitPath[0].length();
-                    // Go into the new dir
-                    // Taking care of the '/' in case there are other sub dirs
+		throw new InvalidDirectoryPathException(
+				"There are no files or directories with name " + currPath);
+	}
 
-                    if (splitPath.length == 1)
-                        return interpretPathRecursive(next,
-                                currPath.substring(length, currPath.length()));
-                    else
-                        return interpretPathRecursive(
-                                next,
-                                currPath.substring(length + 1,
-                                        currPath.length()));
-                }
-            }
+	/**
+	 * Parses the current single dot from currPath and calls interpretPath
+	 * again.
+	 * 
+	 * @param init
+	 *            The dir to interpret the path from.
+	 * @param currPath
+	 *            The current path of the file.
+	 * @param splitPath
+	 *            An array split from currPath with the '/' delimiter;
+	 * @return A file with the given path relative to init;
+	 * @throws InvalidDirectoryPathException
+	 */
+	private static FileTypes interpretSingleDot(Directory init,
+			String currPath, String[] splitPath)
+			throws InvalidDirectoryPathException {
+		// Checking if we have './moredirs/evenmore' or just '.'
+		if (splitPath.length > 1) {
+			return interpretPathRecursive(init,
+					currPath.substring(2, currPath.length()));
+		} else {
+			return interpretPathRecursive(init,
+					currPath.substring(1, currPath.length()));
+		}
+	}
 
-            // Looping over to see if we're looking for a file instead
-            if (splitPath.length == 1) {
-                ArrayList<File> files = init.getChildFiles();
-                Iterator<File> filesIterator = files.iterator();
+	/**
+	 * Parses the double dots from currPath and calls interpretPath again.
+	 * 
+	 * @param init
+	 *            The dir to interpret the path from.
+	 * @param currPath
+	 *            The current path of the file.
+	 * @param splitPath
+	 *            An array split from currPath with the '/' delimiter;
+	 * @return A file with the given path relative to init;
+	 * @throws InvalidDirectoryPathException
+	 */
+	private static FileTypes interpretDoubleDots(Directory init,
+			String currPath, String[] splitPath)
+			throws InvalidDirectoryPathException {
+		// Checking to see if we're at the current root of not
+		if (init.getParent() != null) {
+			// Checking if we have '../moredirs/evenmore' or just '..'
+			if (splitPath.length > 1) {
+				return interpretPathRecursive(init.getParent(),
+						currPath.substring(3, currPath.length()));
+			} else {
+				return interpretPathRecursive(init.getParent(),
+						currPath.substring(2, currPath.length()));
+			}
+		} else {
+			// Checking if we have '../moredirs/evenmore' or just '..'
+			if (splitPath.length > 1) {
+				return interpretPathRecursive(init,
+						currPath.substring(3, currPath.length()));
+			} else {
+				return interpretPathRecursive(init,
+						currPath.substring(2, currPath.length()));
+			}
+		}
+	}
 
-                // Else it's a file we're looking for
-                while (filesIterator.hasNext()) {
-                    File next = filesIterator.next();
+	/**
+	 * Removes the ending slashes in a str i.e.
+	 *
+	 * @param path
+	 * @return
+	 */
+	private static String removeLeadingSlashes(String path) {
+		int slashes = 0;
 
-                    if (!(currPath.lastIndexOf('/') == currPath.length() - 1)) {
-                        if (next.getName().equals(splitPath[0])) {
-                            return next;
-                        }
-                    }
-                }
-            }
+		for (int i = 0; i < path.length(); i++) {
+			if (path.charAt(i) != '/') {
+				break;
+			}
+			slashes++;
+		}
+		return path.substring(slashes, path.length());
+	}
 
-            // Else it doesn't exist and throw an exception
-            throw new InvalidDirectoryPathException(
-                    "There are no files or directories with name " + currPath);
-        }
-        return init;
-    }
+	/**
+	 * Removes the ending slashes in a str i.e.
+	 *
+	 * @param path
+	 * @return
+	 */
+	private static String removeEndingSlashes(String path) {
+		int slashes = 0;
 
-    /**
-     * Removes the ending slashes in a str i.e.
-     *
-     * @param path
-     * @return
-     */
-    private static String removeLeadingSlashes(String path) {
-        int slashes = 0;
+		for (int i = 0; i < path.length(); i++) {
+			if (path.charAt(path.length() - i - 1) != '/') {
+				break;
+			}
+			slashes++;
+		}
+		return path.substring(0, path.length() - slashes);
+	}
 
-        for (int i = 0; i < path.length(); i++) {
-            if (path.charAt(i) != '/') {
-                break;
-            }
-            slashes++;
-        }
-        return path.substring(slashes, path.length());
-    }
+	/**
+	 * @param init
+	 *            The initial dir in which the intrepret path was called in
+	 * @param path
+	 *            The path of the File/Dir TO BE MADE, the last position of the
+	 *            path is to be made and does not exist
+	 * @return The DIR of the path before the file
+	 * @throws InvalidDirectoryPathException
+	 */
+	public static FileTypes interpretMakePath(Directory init, String path)
+			throws InvalidDirectoryPathException {
+		path = removeEndingSlashes(path);
 
-    /**
-     * Removes the ending slashes in a str i.e.
-     *
-     * @param path
-     * @return
-     */
-    private static String removeEndingSlashes(String path) {
-        int slashes = 0;
+		if (path != "") {
+			String[] splitPath = path.split("/");
 
-        for (int i = 0; i < path.length(); i++) {
-            if (path.charAt(path.length() - i - 1) != '/') {
-                break;
-            }
-            slashes++;
-        }
-        return path.substring(0, path.length() - slashes);
-    }
+			int last = splitPath[splitPath.length - 1].length();
 
-    /**
-     * @param init The initial dir in which the intrepret path was called in
-     * @param path The path of the File/Dir TO BE MADE, the last position of the
-     *             path is to be made and does not exist
-     * @return The DIR of the path before the file
-     * @throws InvalidDirectoryPathException
-     */
-    public static FileTypes interpretMakePath(Directory init, String path)
-            throws InvalidDirectoryPathException {
-        path = removeEndingSlashes(path);
+			// if our path has more than 1 subpath e.g. dir/dir1/dir3
+			if (splitPath.length > 2) {
+				return interpretPath(init,
+						path.substring(0, path.length() - last - 1));
+			}
+			// 1 dir like /test
+			return interpretPath(init, path.substring(0, path.length() - last));
+			// if our path has exactly than 1 subpath e.g. dir/dir1/dir3
+		}
+		return init.getParent();
+	}
 
-        if (path != "") {
-            String[] splitPath = path.split("/");
+	public static class InvalidDirectoryPathException extends Exception {
+		private static final long serialVersionUID = 59L;
 
-            int last = splitPath[splitPath.length - 1].length();
-
-            // if our path has more than 1 subpath e.g. dir/dir1/dir3
-            if (splitPath.length > 2) {
-                return interpretPath(init,
-                        path.substring(0, path.length() - last - 1));
-            }
-            // 1 dir like /test
-            return interpretPath(init, path.substring(0, path.length() - last));
-            // if our path has exactly than 1 subpath e.g. dir/dir1/dir3
-        }
-        return init.getParent();
-    }
-
-    ;
-
-    public static class InvalidDirectoryPathException extends Exception {
-        private static final long serialVersionUID = 59L;
-
-        public InvalidDirectoryPathException(String message) {
-            super(message);
-        }
-    }
+		public InvalidDirectoryPathException(String message) {
+			super(message);
+		}
+	}
 }
