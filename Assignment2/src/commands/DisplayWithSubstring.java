6fa095c940afc9872d8317af9ec92dc0c37f40d7
bhuiya16@UTORONTO.CA <bhuiya16@UTORONTO.CA@9bafc4ea-6ec9-488b-8926-2cf38d664e2c>
2016-07-09 10:00:38 +0000

    update sprint 4, 5 new scrum log

--- Assignment2/src/commands/DisplayWithSubstring.java
+++ Assignment2/src/commands/DisplayWithSubstring.java
@@ -35,7 +35,7 @@ public class DisplayWithSubstring implements Command {
 	 */
 	@Override
 	public String man() {
-		return "grep(1)\t\t\t\tUser Commands\t\t\t\tCAT(1)\n"
+		return "GREP(1)\t\t\t\tUser Commands\t\t\t\tGREP(1)\n"
 				+ "\nNAME\n\t\tcat - displays the contents of one or "
 				+ "more files on the standard output\n\nSYNOPSIS\n\t\t"
 				+ "grep FILE1 [FILE2 ...]\n\nDESCRIPTION\n\t\t"
