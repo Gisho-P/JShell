6641d85f68c95b787fd94784984fdd8e90fe195a
pateld59 <pateld59@UTORONTO.CA>
2016-07-10 03:09:33 +0000

    Fixed bugs inmv, cp and curl commands

--- src/commands/MoveFile.java
+++ src/commands/MoveFile.java
@@ -2,15 +2,14 @@ package commands;
 
 import java.util.List;
 
+import driver.FilePathInterpreter;
+import driver.FilePathInterpreter.InvalidDirectoryPathException;
+import driver.MySession;
 import structures.Directory;
 import structures.Directory.InvalidAddition;
 import structures.Directory.MissingNameException;
 import structures.Directory.NameExistsException;
-import structures.File;
 import structures.FileTypes;
-import driver.FilePathInterpreter;
-import driver.FilePathInterpreter.InvalidDirectoryPathException;
-import driver.MySession;
 import structures.Output;
 
 /**
@@ -18,89 +17,92 @@ import structures.Output;
  */
 public class MoveFile implements Command {
 
-	// MySession is used to access the files by finding them through the
-	// root or current directory
-	private MySession s;
-	private Output out;
+    // MySession is used to access the files by finding them through the
+    // root or current directory
+    private MySession s;
+    private Output out;
 
-	public MoveFile(MySession session) {
-		s = session;
-		out = new Output();
-	}
+    public MoveFile(MySession session) {
+        s = session;
+        out = new Output();
+    }
 
-	/**
-	 * Returns the manual for the mv command.
-	 * 
-	 * @return the manual for the mv command
-	 */
-	@Override
-	public String man() {
-		return "mv(1)\t\t\t\tUser Commands\t\t\t\tmv(1)\n"
-				+ "\nNAME\n\t\tmv - displays the contents of one or "
-				+ "more files on the standard output\n\nSYNOPSIS\n\t\t"
-				+ "mv FILE1 [FILE2]\n\nDESCRIPTION\n\t\t"
-				+ "Conmvenates one or more files to the standard output"
-				+ ".\n\t\tCan take any amount of files greater then one as a"
-				+ " parameter.";
-	}
+    /**
+     * Returns the manual for the mv command.
+     *
+     * @return the manual for the mv command
+     */
+    @Override
+    public String man() {
+        return "mv(1)\t\t\t\tUser Commands\t\t\t\tmv(1)\n"
+                + "\nNAME\n\t\tmv - Move file from source to destination. \n" +
+                "\n" +
+                "SYNOPSIS\n" +
+                "\t\t" +
+                "\n\t\t"
+                + "mv FILE1 [FILE2]\n\nDESCRIPTION\n\t\t"
+                + "Move the file from the source path to the \n" +
+                "destination paths if it is valid.";
 
-	/**
-	 * Process arguments passed for mv command and determine whether the
-	 * command was entered correctly or not.
-	 * 
-	 * @param args
-	 *            Arguments parsed from command
-	 * @return The contents of the files given.
-	 */
-	@Override
-	public Output interpret(List<String> args) {
-		if (args.size() != 3) {
-			return out.withStdError("mv usage: mv Src Dest");
-		} else {
-			return exec(args.subList(1, args.size()));
-			// return output from function call
-		}
-	}
+    }
 
-	/**
-	 * Copies the file from one directory to another
-	 * 
-	 * @param args
-	 *            Valid arguments parsed from command
-	 * @return The contents of the files given.
-	 */
-	@Override
-	public Output exec(List<String> args) {
-		try {
-			// Getting both the src and dest place
-			FileTypes src = FilePathInterpreter.interpretPath(s.getCurrentDir(), args.get(0));
-			Directory dest = (Directory) FilePathInterpreter.interpretPath(s.getCurrentDir(), args.get(1));
-			
-			try {
-				Directory parent = (Directory) FilePathInterpreter.interpretMakePath(s.getCurrentDir(), args.get(0));
-				
-				// Removing the previous if it exists
-				if(dest.nameExists(src.getName()) != - 1){
-					dest.remove(src.getName());
-				}
-				
-				// Adding either a dir or a file
-				if(src instanceof File){
-					dest.add(src);
-					parent.remove(src.getName());
-				}
-				else if(src instanceof Directory){
-					dest.add(Directory.deepCopy((Directory)src));
-					parent.remove(src.getName());
-				}
-				
-			} catch (NameExistsException | InvalidAddition | MissingNameException e) {
-				out.addStdError("The file cannot be added either already exists or is not valid.\n");
-			}
-			
-		} catch (InvalidDirectoryPathException e) {
-			out.addStdError("The source path does not exist\n");
-		}
-		return out;
-	}
+    /**
+     * Process arguments passed for mv command and determine whether the command
+     * was entered correctly or not.
+     *
+     * @param args Arguments parsed from command
+     * @return The contents of the files given.
+     */
+    @Override
+    public Output interpret(List<String> args) {
+        if (args.size() != 3) {
+            return out.withStdError("mv usage: mv Src Dest");
+        } else {
+            return exec(args.subList(1, args.size()));
+            // return output from function call
+        }
+    }
+
+    /**
+     * Copies the file from one directory to another
+     *
+     * @param args Valid arguments parsed from command
+     * @return The contents of the files given.
+     */
+    @Override
+    public Output exec(List<String> args) {
+        try {
+            // Getting both the src and dest place
+
+            //still need to do case mv a b where file a's name is changed to b
+            FileTypes src = FilePathInterpreter.interpretPath(s.getCurrentDir(),
+                    args.get(0));
+
+
+            Directory dest = (Directory) FilePathInterpreter.interpretPath(s.getCurrentDir(),
+                    args.get(1));
+
+            try {
+                Directory parent = (Directory) FilePathInterpreter.interpretMakePath(s.getCurrentDir(),
+                        args.get(0));
+
+                // Removing the previous if it exists
+                // Need to double check this
+                if (dest.nameExists(src.getName()) != -1) {
+                    dest.remove(src.getName());
+                }
+
+                dest.add(src);
+                parent.remove(src.getName());
+
+            } catch (NameExistsException | InvalidAddition | MissingNameException e) {
+                out.addStdError("The file cannot be added. It already " +
+                        "exists or is not valid.\n");
+            }
+
+        } catch (InvalidDirectoryPathException e) {
+            out.addStdError("The source path does not exist\n");
+        }
+        return out;
+    }
 }
--- src/commands/RetrieveUrlFile.java
+++ src/commands/RetrieveUrlFile.java
@@ -22,23 +22,43 @@ public class RetrieveUrlFile implements Command {
         this.out = new Output();
     }
 
-    @Override
+    /**
+     * Store the man page for the curl command
+     * @return The string representation of the man page
+     */
     public String man() {
-        return null;
+        return "CURL(1)\t\t\t\tUser Commands\t\t\t\tCURL(1)\n"
+                + "\nNAME\n\t\tcurl - Add the file at the given url "
+                + "to the current directory\n\nSYNOPSIS\n\t\t"
+                + "curl URL\n\nDESCRIPTION\n\t\t"
+                + "Add the file at given url to the current directory."
+                + "\n\t\tThe name of the file is the same as the file in"
+                + " the url.";
     }
 
-    @Override
+    /**
+     * Interpret the command arguments
+     * @param args The arguments given in the curl command
+     * @return Message after the command is executed
+     */
     public Output interpret(List<String> args) {
+        //usage error if incorrect num of args
         if (args.size() != 2) {
             out.withStdError("usage: curl url");
         }
+        //Otherwise execute the command
         return exec(args.subList(1, args.size()));
     }
 
+    /**
+     * Execute the command given the arguments
+     * @param args Arguments used by function to complete command
+     * @return Message related to the command execution
+     */
     public Output exec(List<String> args) {
         //http://www.cs.cmu.edu/~spok/grimmtmp/073.txt
         try {
-            ;
+            //Connect to the url
             URL url = new URL(args.get(0));
             BufferedReader in = new BufferedReader(new InputStreamReader
                     (url.openStream()));
@@ -46,22 +66,22 @@ public class RetrieveUrlFile implements Command {
             //what about urls that end with slashes? Do we have to consider
             // those? What about urls with no files at the end? Or urls with
             // no slashes at all
-            // might have to change names to add
+
+            //Determine the file name to insert in the current directory
             String[] parseUrl = args.get(0).split("/");
             String filename = parseUrl[parseUrl.length - 1];
-            System.out.println(filename);
             File file = new File(filename);
 
             String inputLine;
-
+            //Get data from the url and add it to the file
             while ((inputLine = in.readLine()) != null) {
                 file.appendContent(inputLine);
             }
             in.close();
-
+            //Add the file to the current directory
             session.getCurrentDir().add(file);
 
-            System.out.println(file.getContent());
+            //System.out.println(file.getContent());
         } catch (MalformedURLException e) {
             out.addStdError("Malformed URL.\n");
         } catch (IOException e) {
