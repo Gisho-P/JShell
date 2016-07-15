ef6e4f800b34501cb36e5faffe05e8d982750214
pushpa10@UTORONTO.CA <pushpa10@UTORONTO.CA@9bafc4ea-6ec9-488b-8926-2cf38d664e2c>
2016-07-15 00:44:49 +0000

    Added tests for redirection, and fixed redirection to work for absolute paths

--- Assignment2/src/structures/Output.java
+++ Assignment2/src/structures/Output.java
@@ -13,91 +13,100 @@ import exceptions.InvalidNameException;
 public class Output {
   private String stdOutput;
   private String stdError;
-  
-  public Output(){
+
+  public Output() {
     stdOutput = "";
     stdError = "";
   }
-  
+
   public String getStdOutput() {
-     return stdOutput;
+    return stdOutput;
   }
-  
-  public void addStdOutput(String stdOutput){
+
+  public void addStdOutput(String stdOutput) {
     if (!this.stdOutput.isEmpty()) {
       this.stdOutput += "\n";
     }
     this.stdOutput += stdOutput;
   }
-  
+
   public void setStdOutput(String stdOutput) {
     this.stdOutput = stdOutput;
   }
-  
+
   public void clear() {
     stdOutput = "";
     stdError = "";
   }
 
   public String getStdError() {
-     return stdError;
+    return stdError;
   }
-  
+
   public void setStdError(String standardError) {
     stdError = standardError;
   }
-  
-  public void addStdError(String standardError){
+
+  public void addStdError(String standardError) {
     if (!stdError.isEmpty()) {
       stdError += "\n";
     }
     stdError += standardError;
   }
-  
-  public String getAllOutput(){
-     return getStdError() + getStdOutput();
+
+  public void addErrorClearOutput(String standardError) {
+    addStdError(standardError);
+    setStdOutput("");
   }
-  
-  public void redirect(String file, String type, Directory directory) {
+
+  public String getAllOutput() {
+    return getStdError() + getStdOutput();
+  }
+
+  public void redirect(String file, String type, Directory curDir,
+      Directory rootDir) {
     FileTypes outFile = null;
     // Check if the file exists in the directory
     try {
-      outFile = FilePathInterpreter.interpretPath(directory, file);
-    }
-    // If the file doesn't exist create it
-    catch (InvalidDirectoryPathException e) {
+      outFile = FilePathInterpreter.interpretPath(curDir, file);
+    } catch (InvalidDirectoryPathException e) {
+      // If the file doesn't exist create it
       try {
-        outFile = createFileFromPath(directory, file);
+        outFile = createFileFromPath(curDir, rootDir, file);
       } catch (InvalidDirectoryPathException e1) {
-         addStdError("ERROR: The directory of the file does not exist");
+        addErrorClearOutput("ERROR: The directory of the file does not exist");
       } catch (NameExistsException e1) {
-        addStdError("ERROR: There is already a subdirectory with the same name");
+        addErrorClearOutput(
+            "ERROR: There is already a subdirectory with the same name");
       } catch (InvalidAdditionException e1) {
       } catch (InvalidNameException e1) {
-         addStdError("ERROR: That's an invalid file name");
+        addErrorClearOutput("ERROR: That's an invalid file name");
       }
     }
-    if(outFile instanceof File)
+    if (outFile instanceof File)
       redirectFile((File) outFile, type);
     else
-      addStdError("ERROR: There is already a subdirectory with the same name");
+      addErrorClearOutput(
+          "ERROR: There is already a subdirectory with the same name");
   }
-  
+
   public void redirectFile(File file, String type) {
-    if(file != null) {
+    if (file != null) {
       // Write to the file, overwrite or append as given
       if (type.equals(">"))
         file.setContent(getStdOutput());
       else {
-        file.appendContent(getStdOutput());getClass();
+        file.appendContent(getStdOutput());
+        getClass();
       }
       this.stdOutput = "";
     }
   }
-  
+
   /**
    * Creates the file from the given path, and throws an exception otherwise.
-   * @param directory 
+   * 
+   * @param directory
    *
    * @param path the path to the file
    * @return the file
@@ -107,24 +116,25 @@ public class Output {
    *         name
    * @throws InvalidAdditionException adding a file to the same file
    */
-  private File createFileFromPath(Directory directory, String path) throws InvalidNameException,
-      InvalidDirectoryPathException, NameExistsException, InvalidAdditionException {
+  private File createFileFromPath(Directory curDir, Directory rootDir,
+      String path)
+      throws InvalidNameException, InvalidDirectoryPathException,
+      NameExistsException, InvalidAdditionException {
     File outputFile = new File("");
     // Check if the file is going to be in the current directory
     // or a different directory
     if (((String) path).contains("/")) {
       // If it's a different directory get that directory and add the file
       // to it
-      outputFile = new File(
-          ((String) path).substring(((String) path).lastIndexOf("/") + 1));
-      Directory fileDir =
-          (Directory) FilePathInterpreter.interpretPath(directory,
-              path.substring(0, path.lastIndexOf("/")));
+      outputFile = new File(path.substring(path.lastIndexOf("/") + 1));
+      Directory fileDir = (Directory) FilePathInterpreter
+          .interpretPath(path.startsWith("/") ? rootDir : curDir
+              , path.substring(0, path.lastIndexOf("/")));
       fileDir.add(outputFile);
     } else {
       // If it's the same directory
       outputFile = new File((String) path);
-      directory.add(outputFile);
+      curDir.add(outputFile);
     }
     return outputFile;
   }
