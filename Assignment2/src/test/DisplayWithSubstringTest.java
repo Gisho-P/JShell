3126667e30e6c5253ec20d3a1ea26f3df1704871
songjin8@UTORONTO.CA <songjin8@UTORONTO.CA@9bafc4ea-6ec9-488b-8926-2cf38d664e2c>
2016-07-15 22:33:29 +0000

    Updated and shortened grep length.

--- Assignment2/src/test/DisplayWithSubstringTest.java
+++ Assignment2/src/test/DisplayWithSubstringTest.java
@@ -1,121 +1,98 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.*;
 
 import structures.*;
 import driver.*;
 import exceptions.*;
 
-/**
- * Tests for the grep command.
- */
 public class DisplayWithSubstringTest {
 
-  MySession session;
+	public static MySession session = new MySession(new Output());
 
-  @Before
-  /**
-   * Set up before each test case, create a new session instance
-   */
-  public void setUp() {
-    session = new MySession(new Output());
-  }
+	@After
+	public void tearDown() {
+		session.clearFileSystem();
+		session.clearBuffer();
+		System.out.println(session.getCurrentDir().size());
+	}
 
-  @After
-  /**
-   * Tear down after each test case, clear output/error buffers
-   */
-  public void tearDown() {
-    session.clearBuffer();
-  }
+	/**
+	 * List contents on a file which should return the file name
+	 */
+	@Test
+	public void testFilePathTest() {
+		try {
+			File temp = new File("file1");
+			temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
+			session.getCurrentDir().add(temp);
+		} catch (NameExistsException | InvalidAdditionException
+				| InvalidNameException e) {
+		}
+		JShell.commandProcessor("grep abc.* file1", session);
+		assertEquals("abcdefg\n" + "abcde\n" + "abc", session.returnBuffer());
+	}
 
-  /**
-   * List contents on a file which should return the file name
-   */
-  @Test
-  public void testFilePath() {
-    try {
-      File temp = new File("file1");
-      temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
-      session.getCurrentDir().add(temp);
-    } catch (NameExistsException | InvalidAdditionException
-        | InvalidNameException e) {
-    }
+	/**
+	 * List contents on a file which should return the file name
+	 */
+	@Test
+	public void testRecursivePathTest() {
+		try {
+			File temp = new File("fileone");
+			temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
+			System.out.println(session.getRootDir().size());
+			session.getCurrentDir().add(temp);
+		} catch (NameExistsException | InvalidAdditionException
+				| InvalidNameException e) {
+		}
+		JShell.commandProcessor("grep -R abc.* .", session);
+		assertEquals(
+				"/fileone:abcdefg\n" + "/fileone:abcde\n" + "/fileone:abc",
+				session.returnBuffer());
+	}
 
-    JShell.commandProcessor("grep abc.* file1", session);
-    assertEquals("abcdefg\n" + "abcde\n" + "abc", session.returnBuffer());
-  }
+	@Test
+	public void testAnySubstringTest() {
+		try {
+			File temp = new File("fileone");
+			temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
+			session.getCurrentDir().add(temp);
+		} catch (NameExistsException | InvalidAdditionException
+				| InvalidNameException e) {
+		}
+		JShell.commandProcessor("grep -R .* .", session);
+		assertEquals(
+				"/fileone:abcdefg\n" + "/fileone:abcde\n" + "/fileone:abc",
+				session.returnBuffer());
 
-  /**
-   * List contents on a file which should return the file name, search
-   * recursively through
-   */
-  @Test
-  public void testRecursivePath() {
-    try {
-      File temp = new File("file1");
-      temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
-      session.getCurrentDir().add(temp);
-    } catch (NameExistsException | InvalidAdditionException
-        | InvalidNameException e) {
-    }
+	}
 
-    JShell.commandProcessor("grep -R abc.* file1", session);
-    assertEquals("/:abcdefg\n" + "/:abcde\n" + "/:abc", session.returnBuffer());
-  }
+	@Test
+	public void testSingleSubstringTest() {
+		try {
+			File temp = new File("fileone");
+			temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
+			session.getCurrentDir().add(temp);
+		} catch (NameExistsException | InvalidAdditionException
+				| InvalidNameException e) {
+		}
+		JShell.commandProcessor("grep -R abc .", session);
+		assertEquals("/fileone:abc", session.returnBuffer());
+	}
 
-  /**
-   * Test for using grep to match any string (* character) for files, should
-   * return all files.
-   */
-  @Test
-  public void testAnySubstring() {
-    try {
-      File temp = new File("file1");
-      temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
-      session.getCurrentDir().add(temp);
-    } catch (NameExistsException | InvalidAdditionException
-        | InvalidNameException e) {
-    }
-
-    JShell.commandProcessor("grep -R .* file1", session);
-    assertEquals("/:abcdefg\n" + "/:abcde\n" + "/:abc", session.returnBuffer());
-  }
-
-  /**
-   * Test for grep with no search string provided, should return no files/
-   */
-  @Test
-  public void testEmptySubStringPath() {
-    try {
-      File temp = new File("file1");
-      temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
-      session.getCurrentDir().add(temp);
-    } catch (NameExistsException | InvalidAdditionException
-        | InvalidNameException e) {
-    }
-
-    JShell.commandProcessor("grep -R file1", session);
-    assertEquals("", session.returnBuffer());
-
-  }
-
-  /**
-   * Test for grep using a single search string with no wildcards, should only
-   * match a file with the same string as a file name.
-   */
-  @Test
-  public void testSingleSubstring() {
-    try {
-      File temp = new File("file1");
-      temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
-      session.getCurrentDir().add(temp);
-    } catch (NameExistsException | InvalidAdditionException
-        | InvalidNameException e) {
-    }
-
-    JShell.commandProcessor("grep -R abc file1", session);
-    assertEquals("/:abc", session.returnBuffer());
-  }
+	@Test
+	public void with() {
+		try {
+			File temp = new File("fileone");
+			temp.setContent("abcdefg\n" + "abcde\n" + "abc\n");
+			session.getCurrentDir().add(temp);
+		} catch (NameExistsException | InvalidAdditionException
+				| InvalidNameException e) {
+		}
+		JShell.commandProcessor("grep -R abc .", session);
+		assertEquals("/fileone:abc", session.returnBuffer());
+	}
 }
